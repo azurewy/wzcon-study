bdc751b9af823b047cb9984a4ca9e5d8a5869340
azure <58251204@qq.com>
2019-05-04 22:34:06 +0800

    123

---
+++ wzcon_demo_01/src/main/java/com/wzcon/demo/WzconDemo01Application.java
@@ -0,0 +1,21 @@
+package com.wzcon.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WzconDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WzconDemo01Application.class, args);
+	}
+	
+	@RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+
+}
